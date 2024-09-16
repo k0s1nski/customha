78afc24b1108941b8cb7e88899306a51beea9a3d
K0s1nski <anthony.barbedor@gmail.com>
2024-09-16 14:11:05 +0000

    Rollback config.sh

--- dnsmasqdhcp/rootfs/etc/cont-init.d/config.sh
+++ dnsmasqdhcp/rootfs/etc/cont-init.d/config.sh
@@ -1,15 +1,11 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # DNSMASQ config
 # ==============================================================================
 
+CONFIG="/etc/dnsmasq.conf"
 bashio::log.info "Configuring dnsmasq..."
-
-test1=$(bashio::config 'dhcp_range')
-bashio::log.cyan "Range: $(test1)"
-
-# tempio \
-#     -conf /data/options.json \
-#     -template /usr/share/tempio/dnsmasq.config \
-#     -out /etc/dnsmasq.conf
-
+tempio \
+    -conf /data/options.json \
+    -template /usr/share/tempio/dnsmasq.config \
+    -out "${CONFIG}"
