9d5b09d7ce93616d3c1d183a1a9e686f80139cd0
K0s1nski <anthony.barbedor@gmail.com>
2024-09-16 09:32:06 +0000

    Add debug

--- dnsmasqdhcp/rootfs/etc/cont-init.d/config.sh
+++ dnsmasqdhcp/rootfs/etc/cont-init.d/config.sh
@@ -3,8 +3,9 @@
 # DNSMASQ config
 # ==============================================================================
 
-CONFIG="/etc/dnsmasq.conf"
+CONFIG="/etc/dnsmasq.conf.example"
 bashio::log.info "Configuring dnsmasq..."
+cat /data/options.json
 tempio \
     -conf /data/options.json \
     -template /usr/share/tempio/dnsmasq.config \
