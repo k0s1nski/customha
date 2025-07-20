2bc541a9f4c2139eae458cb26883fc445d32766a
k0sinski <anthony.barbedor@gmail.com>
2025-07-20 16:08:27 +0200

    Sourcing de bashio

--- dnsmasqdhcp/rootfs/etc/cont-init.d/config.sh
+++ dnsmasqdhcp/rootfs/etc/cont-init.d/config.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/with-contenv bash
 # ==============================================================================
 # DNSMASQ and HOSTAPD config
 # ==============================================================================
+source /usr/lib/bashio/bashio.sh
 
 DNS_CONFIG="/etc/dnsmasq.conf"
 bashio::log.info "Configuring dnsmasq..."
@@ -10,9 +11,9 @@ tempio \
     -template /usr/share/tempio/dnsmasq.config \
     -out "${DNS_CONFIG}"
 
-# HOSTAPD_CONFIG="/etc/hostapd.conf"
-# bashio::log.info "Configuring hostapd..."
-# tempio \
-#     -conf /data/options.json \
-#     -template /usr/share/tempio/hostapd.config \
-#     -out "${HOSTAPD_CONFIG}"
+HOSTAPD_CONFIG="/etc/hostapd.conf"
+bashio::log.info "Configuring hostapd..."
+tempio \
+    -conf /data/options.json \
+    -template /usr/share/tempio/hostapd.config \
+    -out "${HOSTAPD_CONFIG}"
