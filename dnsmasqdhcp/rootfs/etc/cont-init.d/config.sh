3176d865d21d830033868150a1765ecfe401f431
k0sinski <anthony.barbedor@gmail.com>
2025-07-20 13:40:32 +0200

    Comment hostapd config bashio

--- dnsmasqdhcp/rootfs/etc/cont-init.d/config.sh
+++ dnsmasqdhcp/rootfs/etc/cont-init.d/config.sh
@@ -10,9 +10,9 @@ tempio \
     -template /usr/share/tempio/dnsmasq.config \
     -out "${DNS_CONFIG}"
 
-HOSTAPD_CONFIG="/etc/hostapd.conf"
-bashio::log.info "Configuring hostapd..."
-tempio \
-    -conf /data/options.json \
-    -template /usr/share/tempio/hostapd.config \
-    -out "${HOSTAPD_CONFIG}"
+# HOSTAPD_CONFIG="/etc/hostapd.conf"
+# bashio::log.info "Configuring hostapd..."
+# tempio \
+#     -conf /data/options.json \
+#     -template /usr/share/tempio/hostapd.config \
+#     -out "${HOSTAPD_CONFIG}"
