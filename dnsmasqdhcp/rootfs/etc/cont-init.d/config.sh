f3335352afd91c6f22960990312cbadc41892b15
k0sinski <anthony.barbedor@gmail.com>
2025-08-13 11:37:01 +0200

    Change to bashio

--- dnsmasqdhcp/rootfs/etc/cont-init.d/config.sh
+++ dnsmasqdhcp/rootfs/etc/cont-init.d/config.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
 # ==============================================================================
 # DNSMASQ and HOSTAPD config
 # ==============================================================================
